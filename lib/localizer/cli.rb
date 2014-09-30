782ca7fcefeaa5a6ce0b932a22b199d0de25319a
Sebastian Skałacki <skalee@gmail.com>
2014-09-30 18:20:35 +0200

    Localizer module split into several files
    More and more needed…

---
+++ lib/localizer/cli.rb
@@ -0,0 +1,62 @@
+module Localizer
+  # When unsure how to modify this class, check: http://whatisthor.com/
+  # or: http://blog.paracode.com/2012/05/17/building-your-tools-with-thor/
+  class CLI < Thor
+
+    long_desc <<-DESC
+Generates translation CSV from given Sencha Ext sources and Java properties.
+
+EXAMPLES
+
+  export -o translations.csv locale_*.properties
+  export -o translations.csv locale-pl.js
+  export -o translations.csv *
+DESC
+    desc "export -o TRANSLATIONS [JAVA_PROPERTIES] [EXT_SOURCES]",
+      "export translations"
+    option :output,
+      type: :string, aliases: ["-o"],
+      desc: "Translations CSV file to be (over)written."
+    def export *source_paths
+      processor = Processor.new
+
+      Dir.glob source_paths do |path|
+        catch :not_recognized do # skip not localizable files
+          locale, type = processor.recognize_file_type path
+          processor.public_send "read_#{type}", locale, path
+        end
+      end
+
+      processor.write_csv options[:output]
+    end
+
+
+    long_desc <<-DESC
+Updates Sencha Ext sources and Java properties according to translations file.
+
+EXAMPLES
+
+  import -i translations.csv locale_*.properties
+  import -i translations.csv locale-pl.js
+  import -i translations.csv *
+DESC
+    desc "import -i TRANSLATIONS [JAVA_PROPERTIES] [EXT_SOURCES]",
+      "import translations"
+    option :input, type: :string, aliases: ["-i"],
+      desc: "CSV file with translations"
+    def import *source_paths
+      processor = Processor.new
+
+      processor.read_csv options[:input]
+
+      Dir.glob source_paths do |path|
+        catch :not_recognized do # skip not localizable files
+          locale, type = processor.recognize_file_type path
+          processor.public_send "write_#{type}", locale, path
+        end
+      end
+    end
+
+  end
+
+end
---
+++ lib/localizer/processor.rb
@@ -0,0 +1,68 @@
+module Localizer
+  class Processor
+
+    Translation = Struct.new :pl, :en
+
+    attr_reader :translations
+
+    def initialize
+      @translations = Hash.new
+    end
+
+    def add_translation locale, key, value
+      entry = translations[key] ||= Translation.new
+      entry[locale] = value
+    end
+
+    def recognize_file_type file_path
+      file_name = File.basename file_path
+
+      match = REX_LOCALE_FILE.match(File.basename file_path)
+
+      throw :not_recognized unless match
+
+      locale = match[:locale].to_sym
+
+      case match[:extension]
+      when "properties" then type = :properties
+      when "js", "javascript" then type = :ext
+      end
+
+      [locale, type]
+    end
+
+    def read_properties locale, file_path
+      JavaProperties.load(file_path).each do |key, value|
+        add_translation locale, key, value
+      end
+    end
+
+    def write_properties locale, file_path
+      translations_in_locale = translations.map do |k,entry|
+        [k, (entry.send locale)]
+      end
+      JavaProperties.write translations_in_locale, file_path
+    end
+
+    def read_ext locale, file_path
+    end
+
+    def read_csv file_path
+      CSV.foreach file_path, headers: :first_row do |row|
+        add_translation :pl, row[0], row[1]
+        add_translation :en, row[0], row[2]
+      end
+    end
+
+    def write_csv file_path
+      csv = CSV.open file_path, "wb"
+      csv << CSV_EXPORT_HEADER
+      translations.each do |key, value|
+        csv << [key, value.pl || "", value.en || ""]
+      end
+    ensure
+      csv && csv.close
+    end
+
+  end
+end
