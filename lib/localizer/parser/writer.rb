7b11b592d2f489a1507c35cbfef6c832656cf3b0
Sebastian Skałacki <skalee@gmail.com>
2014-10-03 04:44:43 +0200

    Do nothing on missing translations for JS sources
    Leave them not translated.

--- lib/localizer/parser/writer.rb
+++ lib/localizer/parser/writer.rb
@@ -3,8 +3,11 @@ module Localizer::Parser::Writer
   # Handles translation in JS source.  Returns translated parameter.
   def translate prefix, key, value
     qualified_key = join_keys prefix, key
-    translated = translations[qualified_key].send locale
-    value.replace_with translated
+    if trnaslation_entry = translations[qualified_key]
+      value.replace_with trnaslation_entry.send locale
+    else
+      value
+    end
   end
 
   # Produces parser output.  In case of Writer, concatenates all the unchanged
